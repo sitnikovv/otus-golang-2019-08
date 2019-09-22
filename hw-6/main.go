c02a3248b496f58b463ce8d47305fba44bc53b9c
Vyacheslav Sitnikov <vsitnikov@gmail.com>
2019-09-22 12:32:03 +0300

    Добашнее задание 6

---
+++ hw-6/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+//	Для проверяющего: я исходил из того, что []List использовать нельзя, хотя с ним было бы куда проще
+
+type List struct {
+	prev *List
+	data interface{}
+	next *List
+}
+
+func (item *List) Prev() (*List, error) {
+	if item.prev == nil {
+		return nil, errors.New("no previous value: it's first element")
+	}
+	return item.prev, nil
+}
+
+func (item *List) Next() (*List, error) {
+	if item.next == nil {
+		return nil, errors.New("no next value: it's last element")
+	}
+	return item.next, nil
+}
+
+func (item *List) Value() interface{} {
+	return item.data
+}
+
+type Glist struct {
+	len   uint  //	Размерность списка
+	first *List //	Ссылка на первый элемент
+	last  *List //	Ссылка на последний элемент
+}
+
+//	Получение размерности списка
+func (g *Glist) Len() uint {
+	return g.len
+}
+
+func (g *Glist) First() *List {
+	return g.first
+}
+
+func (g *Glist) Last() *List {
+	return g.last
+}
+
+func (g *Glist) PushFront(data interface{}) *List {
+	g.len++
+	list := &List{prev: nil, data: data, next: g.first}
+	if g.first == nil {
+		g.first = list
+		g.last = list
+		return list
+	}
+	g.first.prev = list
+	g.first = list
+	return list
+}
+
+func (g *Glist) PushBack(data interface{}) *List {
+	g.len++
+	list := &List{prev: g.last, data: data, next: nil}
+	if g.last == nil {
+		g.first = list
+		g.last = list
+		return list
+	}
+	g.last.next = list
+	g.last = list
+	return list
+}
+
+func (g *Glist) Get(index uint) (*List, error) {
+
+	//	Выходим, если пытаются обратиться к элементу вне списка
+	if index >= g.len {
+		return nil, errors.New("item is missing")
+	}
+
+	var element *List
+	var err error
+
+	//	Подбираемся к удаляемому элементу спереди или сзади, в зависимости от того, откуда ближе
+	if g.len/2 >= index {
+		element = g.First()
+		for i := uint(0); i < index; i++ {
+			if element, err = element.Next(); err != nil {
+				return nil, errors.New("pointer error: " + err.Error())
+			}
+		}
+	} else {
+		element = g.Last()
+		for i := uint(0); i < g.len-index-1; i++ {
+			if element, err = element.Prev(); err != nil {
+				return nil, errors.New("pointer error: " + err.Error())
+			}
+		}
+	}
+	return element, nil
+}
+
+func (g *Glist) Remove(place uint) (*List, error) {
+
+	var (
+		removed, prev, next *List
+		err                 error
+	)
+	if removed, err = g.Get(place); err != nil {
+		return nil, errors.New("no items to remove: " + err.Error())
+	}
+
+	//	Больше ошибок не будет, можно уменьшать счётчик количества
+	g.len--
+
+	//	Если удаляется последний элемент, то сбрасываем указатели и возвращаем удалённый элемент
+	if g.len == 0 {
+		g.first = nil
+		g.last = nil
+		return removed, nil
+	}
+
+	//	Удаление элемента
+	//	Для проверяющего:
+	//	Я знаю, что мог бы обойтись конструкциями removed.prev.next = removed.next, но воспользовался методами List (мало ли, вдруг со временем логика
+	//	поменяется, код не нужно будет переписывать). Так что это не ошибка, а осознанное решение (хотя и оно может быть ошибочным)
+	prev, _ = removed.Prev()
+	next, _ = removed.Next()
+	if prev != nil {
+		prev.next = next
+	}
+	if next != nil {
+		next.prev = prev
+	}
+
+	//	Обрабатываем крайние элементы
+	if g.last == removed {
+		g.last, _ = removed.Prev()
+	}
+	if g.first == removed {
+		g.first, _ = removed.Next()
+	}
+	return removed, nil
+}
+
+func main() {
+	data := Glist{}
+	fmt.Println("Вносим числа от 1 до 10 в начало")
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%d-й пошёл...\n", i)
+		data.PushFront(i)
+	}
+
+	value := data.First()
+	var err error
+	fmt.Printf("Проверяем данные: %d", value.Value())
+	for i := 9; i > 0; i-- {
+		if value, err = value.Next(); err != nil {
+			log.Fatalf("\n\nFail ")
+		} else {
+			fmt.Printf(", %d", value.Value())
+		}
+	}
+	fmt.Println("\n\nВсё хорошо")
+}
---
+++ hw-6/main_test.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+var data = Glist{}
+
+func TestInit(t *testing.T) {
+	if data.first != nil || data.last != nil || data.Len() != 0 {
+		t.Fatalf("Fail initialization list")
+	}
+}
+
+func TestGlist_PushFront(t *testing.T) {
+	data.PushFront(1)
+	if data.first.Value() != 1 || data.last.Value() != 1 || data.Len() != 1 {
+		t.Fatalf("Fail insert first item at begin")
+	}
+
+	data.PushFront(2)
+	if data.first.Value() != 2 || data.last.Value() != 1 || data.Len() != 2 {
+		t.Fatalf("Fail insert new item at begin")
+	}
+}
+
+func TestGlist_PushBack(t *testing.T) {
+	data.PushBack(3)
+	if data.first.Value() != 2 || data.last.Value() != 3 || data.Len() != 3 {
+		t.Fatalf("Fail insert new item at end")
+	}
+}
+
+func TestGlist_First(t *testing.T) {
+	result := data.First().Value()
+	require.Equal(t, result, 2, "Incorrect get first value: waiting 2, getting %v", result)
+}
+
+func TestGlist_Last(t *testing.T) {
+	result := data.Last().Value()
+	require.Equal(t, result, 3, "Incorrect get last value: waiting 3, getting %v", result)
+}
+
+func TestGlist_Get(t *testing.T) {
+
+	//	Тестируем выход за индекс
+	if _, err := data.Get(10); err == nil {
+		require.FailNow(t, "Fail parse error")
+	}
+
+	//	Заполняем данные
+	data.PushBack(4)
+	data.PushBack(5)
+	data.PushBack(6)
+	data.PushBack(7)
+	data.PushBack(8)
+	data.PushBack(9)
+
+	//	Тест чтения нечётного количества элементов
+	result, _ := data.Get(1)
+	require.Equal(t, result.Value(), 1, "Incorrect get last value: waiting 1, getting %v", result.Value())
+	result, _ = data.Get(0)
+	require.Equal(t, result.Value(), 2, "Incorrect get last value: waiting 2, getting %v", result.Value())
+	result, _ = data.Get(2)
+	require.Equal(t, result.Value(), 3, "Incorrect get last value: waiting 3, getting %v", result.Value())
+	result, _ = data.Get(3)
+	require.Equal(t, result.Value(), 4, "Incorrect get last value: waiting 4, getting %v", result.Value())
+	result, _ = data.Get(4)
+	require.Equal(t, result.Value(), 5, "Incorrect get last value: waiting 5, getting %v", result.Value())
+	result, _ = data.Get(5)
+	require.Equal(t, result.Value(), 6, "Incorrect get last value: waiting 6, getting %v", result.Value())
+	result, _ = data.Get(6)
+	require.Equal(t, result.Value(), 7, "Incorrect get last value: waiting 7, getting %v", result.Value())
+	result, _ = data.Get(7)
+	require.Equal(t, result.Value(), 8, "Incorrect get last value: waiting 8, getting %v", result.Value())
+
+	//	Тест чтения чётного количества элементов
+	data.PushBack(10)
+	result, _ = data.Get(1)
+	require.Equal(t, result.Value(), 1, "Incorrect get last value: waiting 1, getting %v", result.Value())
+	result, _ = data.Get(0)
+	require.Equal(t, result.Value(), 2, "Incorrect get last value: waiting 2, getting %v", result.Value())
+	result, _ = data.Get(2)
+	require.Equal(t, result.Value(), 3, "Incorrect get last value: waiting 3, getting %v", result.Value())
+	result, _ = data.Get(3)
+	require.Equal(t, result.Value(), 4, "Incorrect get last value: waiting 4, getting %v", result.Value())
+	result, _ = data.Get(4)
+	require.Equal(t, result.Value(), 5, "Incorrect get last value: waiting 5, getting %v", result.Value())
+	result, _ = data.Get(5)
+	require.Equal(t, result.Value(), 6, "Incorrect get last value: waiting 6, getting %v", result.Value())
+	result, _ = data.Get(6)
+	require.Equal(t, result.Value(), 7, "Incorrect get last value: waiting 7, getting %v", result.Value())
+	result, _ = data.Get(7)
+	require.Equal(t, result.Value(), 8, "Incorrect get last value: waiting 8, getting %v", result.Value())
+	result, _ = data.Get(8)
+	require.Equal(t, result.Value(), 9, "Incorrect get last value: waiting 9, getting %v", result.Value())
+	result, _ = data.Get(9)
+	require.Equal(t, result.Value(), 10, "Incorrect get last value: waiting 10, getting %v", result.Value())
+}
+
+func TestGlist_Remove(t *testing.T) {
+
+	//	Удаляем последний элемент
+	if result, err := data.Remove(9); err != nil {
+		require.FailNow(t, "Fail remove last element, reason: "+err.Error())
+	} else {
+		require.Equal(t, result.Value(), 10, "Remove incorrect element: waiting 10, getting %v", result.Value())
+	}
+	require.Equal(t, data.Len(), uint(9), "Remove incorrect resize after remove: waiting 9, getting %v", data.Len())
+
+	//	Удаляем элемент в середине
+	if result, err := data.Remove(5); err != nil {
+		require.FailNow(t, "Fail remove last element, reason: "+err.Error())
+	} else {
+		require.Equal(t, result.Value(), 6, "Remove incorrect element: waiting 6, getting %v", result.Value())
+	}
+	require.Equal(t, data.Len(), uint(8), "Remove incorrect resize after remove: waiting 8, getting %v", data.Len())
+
+	//	Удаляем элемент в начале
+	if result, err := data.Remove(0); err != nil {
+		require.FailNow(t, "Fail remove last element, reason: "+err.Error())
+	} else {
+		require.Equal(t, result.Value(), 2, "Remove incorrect element: waiting 2, getting %v", result.Value())
+	}
+	require.Equal(t, data.Len(), uint(7), "Remove incorrect resize after remove: waiting 7, getting %v", data.Len())
+
+	//	Удаляем ещё элемент в середине (для проверки целостности)
+	if result, err := data.Remove(5); err != nil {
+		require.FailNow(t, "Fail remove last element, reason: "+err.Error())
+	} else {
+		require.Equal(t, result.Value(), 8, "Remove incorrect element: waiting 8, getting %v", result.Value())
+	}
+	require.Equal(t, data.Len(), uint(6), "Remove incorrect resize after remove: waiting 8, getting %v", data.Len())
+}
